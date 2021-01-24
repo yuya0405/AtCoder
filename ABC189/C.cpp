5e6276eb3bfd032b566bc41d659c85744b6f6192
Yuya Ohbayashi <yuya9445@gmail.com>
2021-01-24 13:11:42 +0900

    ABC189（解き直し・高速化）

--- ABC189/C.cpp
+++ ABC189/C.cpp
@@ -3,11 +3,12 @@ N = int(input())
 l = list(map(int, input().split()))
 
 ans = 0
-for i in range(1, N+1):
-    for j in range(i, N+1):
-        num = min(l[i-1:j])
-        if ans < (j-i+1)*num:
-            ans = (j-i+1)*num
+for i in range(0, N):
+  num = 10**5+1
+  for j in range(i, N):
+      num = min(num, l[j])
+      if ans < (j-i+1)*num:
+          ans = (j-i+1)*num
 print(ans)
 */
 
@@ -24,11 +25,11 @@ int main() {
   }
 
   int ans = 0;
-  int num = 0;
-  for (int i=1; i < N+1; i++){
-    for (int j=i; j < N+1; j++){
-      auto minmax = std::minmax_element(vec.begin()+i-1, vec.begin()+j);
-      num = *minmax.first;
+
+  for (int i = 0; i < N; i++){
+    int num = pow(10,5)+1;
+    for (int j = i; j < N; j++){
+      num = min(num, vec[j]);
       if ( ans < (j-i+1)*num ){
         ans = (j-i+1)*num;
       }
